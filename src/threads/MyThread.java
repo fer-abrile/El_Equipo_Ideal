8648269b9d734eb9ffd933c8fa4ddbd4d99e3a77
Fer Abrile <fer.abrile@gmail.com>
2023-05-26 21:07:55 -0300

    Initial commit + Clases Basicas

---
+++ src/threads/MyThread.java
@@ -0,0 +1,15 @@
+package threads;
+
+public class MyThread extends Thread {
+	
+	@Override
+		public void run() {
+			System.out.println();
+		}
+
+		
+		/* Inicializar Thread para Tracking
+		 * MyThread thread = new MyThread(); 
+		 * thread.start();
+		 */
+}
