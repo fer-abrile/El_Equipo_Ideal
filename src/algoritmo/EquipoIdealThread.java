74b45c41b9215d8bcb61f172a0c38cf276c30cbb
tomas <tomas@tomas-virtualbox>
2023-05-27 21:16:57 -0300

    Algoritmo, thread y clase Personas.
    Se agrega algoritmo en clase EquipoIdeal para conseguir el equipo ideal
    con la lista de personas pasada.
    Se agrega la ejecucion del metodo dentro de un thread.
    Se cambia version a JavaSE-17 por necesidad de usar Comparator.

---
+++ src/algoritmo/EquipoIdealThread.java
@@ -0,0 +1,27 @@
+package algoritmo;
+
+import java.util.*;
+import personas.Persona;
+
+public class EquipoIdealThread implements Runnable {
+    private List<Persona> personas;
+    private List<String[]> incompatibilidades;
+    private Map<String, Integer> roles;
+    private List<Persona> equipoIdeal;
+
+    public EquipoIdealThread(List<Persona> personas, List<String[]> incompatibilidades, Map<String, Integer> roles) {
+        this.personas = personas;
+        this.incompatibilidades = incompatibilidades;
+        this.roles = roles;
+        this.equipoIdeal = new ArrayList<>();
+    }
+
+    public List<Persona> getEquipoIdeal() {
+        return equipoIdeal;
+    }
+
+    @Override
+    public void run() {
+        equipoIdeal = EquipoIdeal.encontrarEquipoOptimo(personas, incompatibilidades, roles);
+    }
+}
--- src/personas/Persona.java
+++ src/personas/Persona.java
@@ -1,5 +1,40 @@
 package personas;
 
 public class Persona {
+    private String nombre;
+    private String rol;
+    private int calificacionHistorica;
 
+    public Persona(String nombre, String rol, int calificacionHistorica) {
+        this.nombre = nombre;
+        this.rol = rol;
+        if(calificacionHistorica > 5 || calificacionHistorica < 1)
+        	throw new IllegalArgumentException("La calificacion historica debe ser mayor que 0 y menor o igual que 5.");
+        this.calificacionHistorica = calificacionHistorica;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+    
+    public int getCalificacionHistorica() {
+        return calificacionHistorica;
+    }
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[Nombre = ");
+		builder.append(nombre);
+		builder.append(", Rol = ");
+		builder.append(rol);
+		builder.append(", Calificacion Historica = ");
+		builder.append(calificacionHistorica);
+		builder.append("]");
+		return builder.toString();
+	}
 }
